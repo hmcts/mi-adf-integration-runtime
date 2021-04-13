dcbabb2618f6993fb1ac5521fe9c089542c8f8ec
tchow8 <tony.chow@hmcts.net>
2021-04-14 00:28:18 +0100

    Changed script to use secrets mount if environment variables are set
    Fix PS conditionals

--- SHIR/setup.ps1
+++ SHIR/setup.ps1
@@ -79,13 +79,27 @@ function RegisterNewNode {
 if (Check-Is-Registered) {
     Write-Log "Restart the existing node"
     Start-Process $DmgcmdPath -Wait -ArgumentList "-Start"
-} elseif (Test-Path Env:AUTH_KEY) {
-    Write-Log "Registering SHIR with the node key: $((Get-Item Env:AUTH_KEY).Value)"
-    Write-Log "Registering SHIR with the node name: $((Get-Item Env:NODE_NAME).Value)"
-    Write-Log "Registering SHIR with the enable high availability flag: $((Get-Item Env:ENABLE_HA).Value)"
-    Write-Log "Registering SHIR with the tcp port: $((Get-Item Env:HA_PORT).Value)"
+} elseif ((Test-Path Env:AUTH_KEY) -Or ((Test-Path Env:SECRETS_MOUNT_PATH) -And (Test-Path Env:SECRETS_AUTH_KEY_NAME))) {
+    $IRAuthKey = (Get-Item Env:AUTH_KEY).Value
+    $IRNodeName = (Get-Item Env:NODE_NAME).Value
+    $IREnableHA = (Get-Item Env:ENABLE_HA).Value
+    $IRHAPort = (Get-Item Env:HA_PORT).Value
+
+    # Secrets mount value takes priority over environment variable setting for security.
+    if ((Test-Path Env:SECRETS_MOUNT_PATH) -And (Test-Path Env:SECRETS_AUTH_KEY_NAME)) {
+        $SecretsMountPath = (Get-Item Env:SECRETS_MOUNT_PATH).Value
+        $SecretsAuthKeyName = (Get-Item Env:SECRETS_AUTH_KEY_NAME).Value
+        Write-Log "Getting AuthKey from KeyVault Mount: $($SecretsMountPath)"
+        Write-Log "Using secret name: $($SecretsAuthKeyName)"
+        $IRAuthKey = Get-Content "$($SecretsMountPath)\$($SecretsAuthKeyName)"
+    }
+
+    Write-Log "Registering SHIR with the node key: $($IRAuthKey)"
+    Write-Log "Registering SHIR with the node name: $($IRNodeName)"
+    Write-Log "Registering SHIR with the enable high availability flag: $($IREnableHA)"
+    Write-Log "Registering SHIR with the tcp port: $($IRHAPort)"
     Start-Process $DmgcmdPath -Wait -ArgumentList "-Start"
-    RegisterNewNode (Get-Item Env:AUTH_KEY).Value (Get-Item Env:NODE_NAME).Value (Get-Item Env:ENABLE_HA).Value (Get-Item Env:HA_PORT).Value
+    RegisterNewNode $IRAuthKey $IRNodeName $IREnableHA $IRHAPort
 } else {
     Write-Log "Invalid AUTH_KEY Value"
     exit 1
