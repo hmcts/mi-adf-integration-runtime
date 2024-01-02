4ab30c5567f5b6ccbfc5882f8ad4b05040e8bcfd
Tony Chow <tony.chow@hmcts.net>
2024-01-02 09:37:36 +0000

    Update health-check script to have more conditions

--- SHIR/setup.ps1
+++ SHIR/setup.ps1
@@ -4,48 +4,6 @@ Import-Module $PSScriptRoot\secrets-setup.ps1
 
 $DmgcmdPath = "C:\Program Files\Microsoft Integration Runtime\5.0\Shared\dmgcmd.exe"
 
-function Check-Is-Registered() {
-    $result = Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\DataTransfer\DataManagementGateway\ConfigurationManager' -Name HaveRun -ErrorAction SilentlyContinue
-    if (($result -ne $null) -and ($result.HaveRun -eq 'Mdw')) {
-        return $TRUE
-    }
-    return $FALSE
-}
-
-function Check-Main-Process() {
-    $ProcessResult = Get-WmiObject Win32_Process -Filter "name = 'diahost.exe'"
-    
-    if ($ProcessResult) {
-        return $TRUE
-    }
-
-    Write-Log-Error "diahost.exe is not running"
-    return $FALSE
-}
-
-function Get-Connection-Status() {
-    $processStartInfo = New-Object System.Diagnostics.ProcessStartInfo
-    $processStartInfo.FileName = $DmgcmdPath
-    $processStartInfo.RedirectStandardError = $true
-    $processStartInfo.RedirectStandardOutput = $true
-    $processStartInfo.UseShellExecute = $false
-    $processStartInfo.Arguments = "-cgc"
-    
-    $process = New-Object System.Diagnostics.Process
-    $process.StartInfo = $processStartInfo
-    $process.Start() | Out-Null
-    $process.WaitForExit()
-    
-    $ConnectionResult = $process.StandardOutput.ReadToEnd() -replace "`t|`n|`r",""
-    $ConnectionError = $process.StandardError.ReadToEnd()
-    
-    if ($ConnectionError) {
-        Write-Log-Error "Error raised: $($ConnectionError)"
-    }
-    
-    return $ConnectionResult
-}
-
 function RegisterNewNode {
     Param(
         $AUTH_KEY,
